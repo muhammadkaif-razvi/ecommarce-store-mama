b11be4595c158068bf608c95bd9a80157b56c4dd
muhammadkaif-razvi <muhammadkaifrazwi@gmail.com>
2025-02-24 12:54:59 +0530

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -28,9 +28,12 @@ export default function Home() {
             className="flex items-center space-x-4 rtl:space-x-reverse"
           >
             <Image
-              src="https://auth.js.org/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.048131a8.png&w=384&q=75"
+              src="/globe.svg"
               className="h-8"
               alt="authjs logo"
+              width={100}
+              height={100}
+              priority
             />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               authjs
--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone", // Ensures Node.js runtime, not Edge
   images: {
-    domains: ["static.vecteezy.com"],
-  },};
+    domains: ["static.vecteezy.com",],
+  },
+  experimental: {
+    serverActions: { bodySizeLimit: "2mb" }, // Optional
+  },
+};
 
 export default nextConfig;
