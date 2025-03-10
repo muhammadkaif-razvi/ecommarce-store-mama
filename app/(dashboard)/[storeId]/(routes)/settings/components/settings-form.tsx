8dfa0fdb47d57b044e0a6f5a7330f198dadc417f
muhammadkaif-razvi <muhammadkaifrazwi@gmail.com>
2025-03-11 02:08:00 +0530

    store props fix

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,13 +1,15 @@
 "use client";
+import { Heading } from "@/components/ui/heading";
 import { Store } from "@prisma/client";
 
 interface SettingsFormProps {
-  initialData: Store
+  initialData: Store;
 }
-export const SettingsForm: React.FC<SettingsFormProps> = ({
-  initialData
-})=>{
-  return (
-    <div>SettingsForm</div>
-  )
-}
+export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+  return <div className="flex items-center justify-between">
+    <Heading 
+    title="Settings"
+    description="Manage store preferences"/>
+    SettingsForm
+    </div>;
+};
