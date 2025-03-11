78d3fef478dd55f87daa2eb69fd1442d7152dd57
muhammadkaif-razvi <muhammadkaifrazwi@gmail.com>
2025-03-11 16:51:08 +0530

    kamit

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -2,19 +2,20 @@ import { currentUser } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 
-
 interface DashboardPageProps {
   params: {
     storeId: string;
   };
 }
 
-async function  DashboardPage({
-  params,
-}:DashboardPageProps)  {
-
-const { storeId } = await params;
+async function DashboardPage({ params }: DashboardPageProps) {
+  const storeId = (await params).storeId;
   const user = await currentUser();
+
+  if (!user) {
+    redirect("/login"); // Redirect if the user is not authenticated
+  }
+
   const store = await db.store.findFirst({
     where: {
       id: storeId,
@@ -23,13 +24,14 @@ const { storeId } = await params;
   });
 
   if (!store) {
-    redirect("/stores-setup");
+    redirect("/stores-setup"); // Redirect if the store is not found
   }
+
   return (
     <div className="mt-20 font-semibold">
       {store ? `Active Store: ${store.name}` : "Store Not Found"}
     </div>
   );
-};
+}
 
 export default DashboardPage;
