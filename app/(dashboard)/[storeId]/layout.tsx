1914cba93e235d24c3a1b3e9f88453354b621b0e
muhammadkaif-razvi <muhammadkaifrazwi@gmail.com>
2025-03-12 02:05:36 +0530

    refactor: update params type to Promise in DashboardLayout and implement alert components

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -2,20 +2,22 @@ import { currentUser } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 
+
+
 export default async function DashboardLayout({
   children,
   params,
 }: {
   children: React.ReactNode;
-  params : {storeId: string};
+  params: Promise<{ storeId: string}>
 }) {
   const user = await currentUser();
+  const { storeId } = await params;
 
   if (!user) {
     redirect("/login");
   }
 
-const {storeId} = await params
   
 
   const store = await db.store.findFirst({
