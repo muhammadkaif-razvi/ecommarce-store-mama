580d7163c50fdd91bc7955b8753d5afe6065e465
muhammadkaif-razvi <muhammadkaifrazwi@gmail.com>
2025-05-11 20:01:03 +0530

    feat: implement getCustomer function to fetch customer data by ID

--- app/(routes)/account/orders/page.tsx
+++ app/(routes)/account/orders/page.tsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import getCustomer from "@/actions/getdatafromAdmin/get-customer"
+import { currentUser } from "@/lib/auth"
 
-const OrderPage = () => {
+
+
+const OrderPage = async () => {
+  const user = await currentUser();
+  const customer = await getCustomer(user.id);
+  console.log(customer);
   return (
     <div className='mt-20'>OrderPage</div>
   )
