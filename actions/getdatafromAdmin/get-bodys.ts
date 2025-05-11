124e3ae8ab3ef9e65c1357998fee732193e4146c
muhammadkaif-razvi <muhammadkaifrazwi@gmail.com>
2025-05-11 19:47:43 +0530

    refactor: update getBodys function to use Body type and remove unused cart API routes

--- actions/getdatafromAdmin/get-bodys.ts
+++ actions/getdatafromAdmin/get-bodys.ts
@@ -1,8 +1,8 @@
-import { Face } from "@/types";
+import { Body} from "@/types";
 
 const URL  = `${process.env.NEXT_PUBLIC_API_URL}/bodys`;
 
-const getBodys = async (): Promise<Face[]> => {
+const getBodys = async (): Promise<Body[]> => {
   const res = await fetch(URL);
     
   return res.json();
